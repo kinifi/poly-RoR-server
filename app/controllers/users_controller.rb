2f7cb827fb55f6aad45a173b80bf4b25070e72af
kinifi <chrisfigueroa16@gmail.com>
2017-01-20 14:34:14 -0800

    finished user login and sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,18 +22,14 @@ class UsersController < ApplicationController
   end
 
   # POST /users
-  # POST /users.json
   def create
     @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.save
+      log_in @user
+      flash[:success] = "Welcome to Poly!"
+      redirect_to @user
+    else
+      render 'new'
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,21 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
   root 'static_pages#home'
   get '/home', to: 'static_pages#home'
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/search', to: 'polies#index'
+  #poly creation and listing
   get '/polies', to: 'polies#index'
   get '/polies/new', to: 'polies#new'
+  #user creation and login
   get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 
   resources :users
   resources :polies
