b8a543d77162bd70f131b99dffbb494753ea4ba4
kinifi <chrisfigueroa16@gmail.com>
2017-01-22 14:18:32 -0800

    no idea if this is going to fix things

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,8 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'support@poly.gg'
-  
-  def welcome_email(user)
-    @user = user
-    mail(to: @user.email, subject: 'Sample Email')
-  end
+  layout 'mailer'
 end
