e6c1c0002aa9bdaae151b3fc199d948034fd8165
kinifi <chrisfigueroa16@gmail.com>
2017-01-25 22:47:31 -0800

    testing email out

--- app/controllers/password_reset_controller.rb
+++ app/controllers/password_reset_controller.rb
@@ -3,7 +3,17 @@ class PasswordResetController < ApplicationController
   def reset
     user = User.find_by(email: params[:email])
     if user
-      flash.now[:danger] = 'Password emailed to account' + user.email
+      flash.now[:danger] = 'Password emailed to account'
+      #the postmark email method
+      # Create an instance of Postmark::ApiClient
+      client = Postmark::ApiClient.new(ENV['POSTMARK'])
+      client.deliver_with_template(from: 'support@poly.gg',
+                               to: user.name + ' <' + @user.email + '>',
+                               template_id: 1258543,
+                               template_model: {
+                                 name: user.name,
+                                 password: user.email
+                               })
     else
       # Create an error message.
       flash.now[:danger] = 'Invalid Email'
